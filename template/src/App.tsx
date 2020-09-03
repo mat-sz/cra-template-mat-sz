229f74a9ab556adb85e6cc9b34873b728974d08c
Mat Sz <contact@matsz.dev>
2020-09-03 18:18:16 +0200

    added router to App.tsx

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.scss';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>Hello, world!</h1>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/">
+            <h1>Hello, world!</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
